2a2309c21f67d0a675601f4290acd2f15ab1d872
Lars T Hansen <lth@acm.org>
2014-11-24 10:56:22 +0100

    Iterated mandelbrot (easy)

---
+++ mandelbrot-iterated/mandelbrot-master.js
@@ -0,0 +1,41 @@
+const numWorkers = 4;
+const maxIterations = 120;
+const animate = true;
+
+Multicore.init(numWorkers, "mandelbrot-worker.js", doMandelbrot);
+
+const rawmem = new SharedArrayBuffer(height*width*4*2);
+const mem1 = new SharedInt32Array(rawmem, 0, height*width);
+const mem2 = new SharedInt32Array(rawmem, height*width, height*width);
+
+var magnification = 1;
+var iterations = 0;
+var mem = mem1;
+var timeBefore;
+
+function doMandelbrot() {
+    Multicore.build(showMandelbrot, "mandelbrot", mem, [[0,height], [0,width]], magnification);
+}
+
+function showMandelbrot() {
+    var memnow = mem;
+    var display = animate;
+    if (iterations == 0)
+	timeBefore = Date.now();
+    if (iterations < maxIterations) {
+	iterations++;
+	magnification *= 1.1;
+	mem = (memnow == mem1) ? mem2 : mem1;
+	doMandelbrot();
+    }
+    else {
+	var t = Date.now() - timeBefore;
+	console.log("Number of workers: " + numWorkers + "  Compute time: " + t + "ms  FPS=" + (iterations/(t/1000)));
+	display = true;
+    }
+    if (display) 
+	canvasSetFromABGRBytes(document.getElementById("mycanvas"),
+			       new SharedUint8Array(rawmem, memnow.byteOffset, height*width*4),
+			       height,
+			       width);
+}
---
+++ mandelbrot-iterated/mandelbrot-worker.js
@@ -0,0 +1,40 @@
+importScripts("../util/barrier.js",
+	      "../util/parinvoke-worker.js",
+	      "mandelbrot-parameters.js");
+
+Multicore.addFunction("mandelbrot", mandelbrot);
+
+// Maximum iterations per pixel.
+const MAXIT = 1000;
+
+// Colors are ABGR with A=255.
+const colors = [0xFFFF0700, 0xFF2a2aa5, 0xFFFFff00, 0xFFa19eff,
+		0xFF00eefd, 0xFF008000, 0xFFFAFEFE, 0xFF00FFBF];
+
+// Compute a square of pixels into mem with y in [ybase, ylimit)
+// and x in [xbase, xlimit).
+
+function mandelbrot(mem, ybase, ylimit, xbase, xlimit, magnification) {
+    if (!(mem instanceof SharedInt32Array) || mem.length != height*width)
+	throw new Error("Bad object: " + mem + " " + mem.length);
+    const g_top = g_center_y + 1/magnification;
+    const g_bottom = g_center_y - 1/magnification;
+    const g_left = g_center_x - width/height*1/magnification;
+    const g_right = g_center_x + width/height*1/magnification;
+    for ( var Py=ybase ; Py < ylimit ; Py++ ) {
+	for ( var Px=xbase ; Px < xlimit ; Px++ ) {
+	    var x0 = g_left+(Px/width)*(g_right-g_left);
+	    var y0 = g_bottom+(Py/height)*(g_top-g_bottom);
+	    var x = 0.0;
+	    var y = 0.0;
+	    var it = 0;
+	    while (x*x + y*y < 4.0 && it < MAXIT) {
+		var xtemp = x*x - y*y + x0;
+		y = 2.0*x*y + y0;
+		x = xtemp;
+		it++;
+	    }
+	    mem[Py*width+Px] = it == MAXIT ? 0xFF000000 : colors[it & 7];
+	}
+    }
+}
