83e1efd39b0049fc3407d93c983548472140ad5a
Lars T Hansen <lth@acm.org>
2014-12-01 10:24:35 -0600

    Misc drafts

--- draft/asymmetric-lock.js
+++ draft/asymmetric-lock.js
@@ -1,19 +1,50 @@
 // MasterLock / WorkerLock is a an asymmetric lock data structure.
+// 2014-11-28 / lhansen@mozilla.com.
 //
-// The master will call the requestLock() method on the lock, and this
-// will register a callback to be called with the lock acquired.
+// The master will call the asyncLock() method on the lock, and this
+// will register a callback to be called with the lock acquired.  The
+// master cannot unlock the lock explicitly, it will be unlocked when
+// the callback returns.
 //
 // The worker will call the lock() method on the lock, and this blocks
-// until the lock can be acquired.
+// until the lock can be acquired.  The worker unlocks the lock
+// explicitly.
 //
-// REQUIREMENT ON WORKERS:
 //
-// All message loops installed on workers MUST respond to a message of
-// the following form:
+// REQUIREMENT ON THE MASTER-SIDE WORKERS' MESSAGE LOOPS:
+//
+// All message loops installed by the master on workers MUST respond
+// to a message of the following form:
 //
 //   ["MasterLock.dispatch", v]
 //
 // by calling MasterLock.dispatch(v).
+//
+//
+// TODO:
+//
+// - It'd be desirable for the callback to be allowed to perform an
+//   explicit unlock, thus exiting the critical section early, and
+//   equally desirable for it to be allowed to set up a new callback
+//   if it's outside the critical section.  (At a guess, we can do
+//   something with a sequence number in the lock; unlock would
+//   increment it, and if it has changed when the callback exits we
+//   can skip the unlock.)
+//
+//
+// LOCK REPRESENTATION:
+//
+// The lock occupies three consecutive words.
+//
+//   @0: lock state (0, 1, 2)
+//   @1: main thread state (0, 1)
+//   @2: global lock ID, set by the master
+//
+// The lock code is based on http://www.akkadia.org/drepper/futex.pdf.
+// Lock state values:
+//   0: unlocked
+//   1: locked with no waiters
+//   2: locked with possible waiters
 
 
 // Create the Master side of the lock.
@@ -36,24 +67,27 @@ function MasterLock(iab, offset) {
     iab[offset+2] = this._id;
 }
 
-// @0: lock state (0, 1, 2)
-// @1: main thread state (0, 1)
-// @2: lock ID
-//
-// The lock code is based on http://www.akkadia.org/drepper/futex.pdf.
-//
-// Lock state values:
-//   0: unlocked
-//   1: locked with no waiters
-//   2: locked with possible waiters
+// Constant.  Number of consecutive locations that must be allocated
+// for the lock data structure.
 
 MasterLock.NUMLOCS = 3;
 
+// PRIVATE.  Uniquely identifies the lock for comm purposes.
+
 MasterLock._id = 1;
+
+// PRIVATE.  Map from ID to lock structure for any locks waiting in
+// the master.
+
 MasterLock._waiting = {};
 
-// Callback from a worker.  The lock is still held, ownership is
-// transfered to the master.
+// Process a callback from a worker.  Invoke this in the master from
+// the worker's onmessage handler when the message has the form
+// ["MasterLock.dispatch", v].
+//
+// When this is invoked the worker will be holding the lock identified
+// by v; invoking this function has the effect of transfering the lock
+// to the master.
 
 MasterLock.dispatch =
     function (id) {
@@ -65,7 +99,7 @@ MasterLock.dispatch =
 	    cb = lock._callback;
 	    lock._callback = null;
 	    if (cb)
-		cb();
+		cb(false);
 	    lock._unlock();
 	}
 
@@ -73,62 +107,73 @@ MasterLock.dispatch =
 	    throw new Error("Internal error: No callback for " + id);
     };
 
-// requestLock(cb) registers an interest in the lock with cb being the
+// asyncLock(cb) registers an interest in the lock with cb being the
 //   callback to be invoked once the lock is acquired.  If the lock
 //   can be acquired directly then the callback may be invoked directly.
 //
-// Returns true if the lock was acquired immediately and the callback
-// was invoked and hence no callback became registered, and false
-// otherwise.
+//   The callback is passed a flag, which is true if the lock was
+//   acquired directly and the callback invoked synchronously.
+//
+// Returns the value of the flag passed to the callback.
 
-MasterLock.prototype.requestLock =
+MasterLock.prototype.asyncLock =
     function (callback) {
         const iab = this._iab;
         const index = this._offset;
 	const master = this._offset+1;
-        var c = 0;
 
 	// Register interest in the lock.  The old 'master' value will
-	// always be zero here.
+	// always be zero here because asyncLock cannot be called from
+	// within a callback.
 	Atomics.store(iab, master, 1);
 
-	while (true) {
-            if ((c = Atomics.compareExchange(iab, index, 0, 1)) == 0) {
-		// Lock acquired, but maybe a message was sent anyway?
-		if (Atomics.compareExchange(iab, master, 1, 0) == 1) {
-		    // Nobody dispatched anything and we disinterested ourselves.
-		    callback();
-		    this._unlock();
-		    return true;
-		}
-
-		// Bad logic here: if a message was sent then we will
-		// not acquire the lock above because the lock was
-		// retained for us. 
-		//
-		// FIXME
-
-		// A message was sent between the setting of the flag
-		// and the acquire of the lock (could take multiple
-		// iterations of this loop too).  Set up the callback
-		// to capture the message.  Retain the lock.
-		this._callback = callback;
-		MasterLock._waiting[this._id] = this;
-		return false;
+	do {
+	    // Try to acquire the lock.  If we did then we can just
+	    // unregister interest and go ahead and call the callback,
+	    // because if a worker had sent a message then it would
+	    // also have retained the lock.
+            if (Atomics.compareExchange(iab, index, 0, 1) == 0) {
+		if (Atomics.compareExchange(iab, master, 1, 0) == 0)
+		    throw new Error("FATAL ERROR: Inconsistent state"); // Nobody should have reset
+		callback(true);
+		this._unlock();
+		return true;
 	    }
-	    else {
-		// Lock not acquired but we want to acquire it.  Lock
-		// could be released by now, so check and redo if so.
-		if (Atomics.compareExchange(iab, index, 1, 2) == 0)
-		    continue;
 
-		// Lock not held, multiple waiters and the master flag
-		// are set, and the callback is registered.
-		this._callback = callback;
-		MasterLock._waiting[this._id] = this;
-		return false;
-	    }
-        }
+	    // Register that we want to acquire the lock, which we
+	    // found to be held.  It could have been released by its
+	    // single owner without a message being sent, so start
+	    // over if that seems to be the case.
+	} while (Atomics.compareExchange(iab, index, 1, 2) == 0);
+
+	// We have registered interest in the lock, we're not
+	// holding it, and the state of the lock will ensure that
+	// a release will cause a message to be sent when the lock
+	// becomes available.
+	this._callback = callback;
+	MasterLock._waiting[this._id] = this;
+	return false;
+    };
+
+// tryLock(cb) attempts to obtain the lock, and if it does then the
+//   "callback" cb is invoked directly.  The callback is always
+//   passed a flag whose value is true.
+//
+// Returns true if the lock was acquired immediately and the callback
+// was invoked, and false otherwise.
+
+MasterLock.prototype.tryLock =
+    function (callback) {
+        const iab = this._iab;
+        const index = this._offset;
+
+        if (Atomics.compareExchange(iab, index, 0, 1) == 0) {
+	    callback(true);
+	    this._unlock();
+	    return true;
+	}
+
+	return false;
     };
 
 // PRIVATE.  Unlock the lock and wake any waiters.  The callback shall
@@ -156,6 +201,8 @@ function WorkerLock(iab, offset) {
     this._offset = offset;
 }
 
+// Acquire the lock, blocking until the lock becomes available.
+
 WorkerLock.prototype.lock =
     function () {
         const iab = this._iab;
@@ -169,6 +216,24 @@ WorkerLock.prototype.lock =
             } while ((c = Atomics.compareExchange(iab, index, 0, 2)) != 0);
         }
     };
+
+// Attempt to acquire the lock, but do not block.  Return true if the
+// lock was acquired, otherwise false.
+
+WorkerLock.prototype.tryLock =
+    function () {
+        const iab = this._iab;
+        const index = this._offset;
+        return Atomics.compareExchange(iab, index, 0, 1) == 0;
+    };
+
+// Unlock a lock that's held and wake up another agent if there's one
+// waiting.
+//
+// NOTE: The implementation currently prioritizes waking up the
+// master, but there should be no correctness issues about choosing a
+// worker over the master even if the master is waiting.  As it is,
+// there's a risk of starving the workers.
 
 WorkerLock.prototype.unlock =
     function () {
---
+++ draft/test-lock-master.js
@@ -0,0 +1,46 @@
+var numWorkers = 4;
+var iab = new SharedInt32Array(1024*100);
+var lockLoc = 0;
+var flagLoc = lockLoc + MasterLock.NUMLOCS;
+var compLoc = flagLoc + 1;
+var compSiz = iab.length - iab.compLoc;
+var l = new MasterLock(iab, 0);
+
+for ( var i=0 ; i < numWorkers ; i++ ) {
+    var w = new Worker("test-lock-worker.js");
+    w.postMessage(["start", iab.buffer, lockLoc, flagLoc, compLoc, compSiz], [iab.buffer]);
+}
+
+setTimeout(function () {
+    flagLoc = -1;		// Start the workers, who are busy-waiting
+    performWork();
+}, 0);
+
+var nextIndex = 0;
+var a = 3;
+var b = -4;
+var c = 2;
+function performWork() {
+    // This is a typical optimized critical section loop: the while
+    // loop tests the flag and will perform another iteration if
+    // the lock was taken synchronously; the callback will restart the
+    // loop at the end of the critical section if the lock had to be
+    // taken by a callback.
+    while (nextIndex < compSiz &&
+	   l.asyncLock(function (sync) {
+	       // Increment by 1
+	       iab[compLoc+nextIndex]+=a;
+	       iab[compLoc+nextIndex]+=b;
+	       iab[compLoc+nextIndex]+=c;
+	       nextIndex++;
+	       if (!sync)
+		   setTimeout(performWork, 0);
+	   }))
+    {
+	// Body must be empty
+    }
+    if (nextIndex == compSiz) {
+	// Now wait until the workers are all idle, there needs to be a counter loc
+	// Then check all the values
+    }
+}
